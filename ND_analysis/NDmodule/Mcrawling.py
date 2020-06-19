c937ebf9b91083e2900ac218f7eb22184331ee2f
bluecat222 <guswhd2601@naver.com>
2020-06-19 12:18:57 +0900

    commit complete

--- ND_analysis/NDmodule/Mcrawling.py
+++ ND_analysis/NDmodule/Mcrawling.py
@@ -61,3 +61,53 @@ def day_chart(db, year, S_month = 1, E_month = 12):
                 db.commit()
 
                 rank += 1
+
+
+def rank_gap(db, year, S_month = 1, E_month = 12):
+    import requests
+    import calendar  # .monthrange(year, month) : 해당년도, 해당월의 날짜수
+    from bs4 import BeautifulSoup  # html 분석
+
+    def getPageString(url):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'
+        }
+        data = requests.get(url, headers=headers)
+        return data.content
+
+    cursor = db.cursor()
+
+    for month in range(S_month, E_month + 1):  # 차트's month
+        for day in range(1, calendar.monthrange(year, month)[1] + 1):  # 차트's day
+
+            gaplist = []
+
+            date = str(year) + '-' + str(month) + '-' + str(day)  # 차트 날짜
+            print(date)
+
+            for p in range(1,3):
+                url1 = "https://www.genie.co.kr/chart/top200?ditc=D&ymd=" + str(year) + str(month).zfill(2) + str(day).zfill(2) + "&hh=16&rtm=N&pg=" + str(p)  # 일별차트 url
+                pageString = getPageString(url1)
+
+                bsObj = BeautifulSoup(pageString, "html.parser")
+
+                numberlist = bsObj.find_all("td", {"class": "number"})
+
+                for number in numberlist[:50]:
+                    try:
+                        gap = number.find("span", {"class": "rank-up"}).text
+                    except AttributeError:
+                        gap = "0"
+
+                    gaplist.append(gap.replace("상승",""))
+
+            rank = 1
+
+            sql = "UPDATE musicl SET rankgap = %s WHERE date = %s AND ranking = %s"
+
+            for i in range(100):
+                cursor.execute(sql, (gaplist[i], date, rank))  # 크롤링 데이터 DB에 입력
+
+                db.commit()
+
+                rank += 1
